d13c5e7601b59c7690e61ee9243af8b00fe84cf1
Tad Marshall <tad@10gen.com>
2012-03-13 14:07:48 -0400

    Fleshed out readUnicodeCharacter()

--- src/third_party/linenoise/linenoise.cpp
+++ src/third_party/linenoise/linenoise.cpp
@@ -833,13 +833,68 @@ void InputBuffer::refreshLine( PromptBase& pi ) {
  * @return  UChar32 Unicode character
  */
 
-static UChar32 readUnicodeCharacter( void ) {
-    UChar8 c;
 #ifdef _WIN32
 #define read _read
 #endif
-    if ( read( 0, &c, 1 ) <= 0 ) return 0;
-    return c;
+
+static UChar32 readUnicodeCharacter( void ) {
+#if 1
+    UChar8 c;
+    static UChar8 utf8String[5];
+    static int utf8Count = 0;
+    UChar32 unicodeChar[2];
+    size_t ucharCount;
+    int errorCode = -1;
+    while ( errorCode ) {
+        if ( read( 0, &c, 1 ) <= 0 ) return 0;
+        if ( c <= 0x7F ) {      // short circuit ASCII
+            utf8Count = 0;
+            return c;
+        }
+        else if ( utf8Count < sizeof( utf8String ) - 1 ) {
+            utf8String[ utf8Count++ ] = c;
+            utf8String[ utf8Count ] = 0;
+            copyString8to32( unicodeChar, utf8String, 2, ucharCount, errorCode );
+            if ( ucharCount && errorCode == 0 ) {
+                utf8Count = 0;
+                return unicodeChar[0];
+            }
+        }
+        else {
+            // this really shouldn't happen ... got four bytes but no UTF-8 character ...
+            utf8Count = 0;
+        }
+    }
+#else
+    UChar8 c;
+    static UChar8 utf8String[5];
+    static int utf8Count = 0;
+    UChar32 unicodeChar[2];
+    size_t ucharCount;
+    int errorCode = -1;
+    while ( errorCode ) {
+        if ( read( 0, &c, 1 ) <= 0 ) return 0;
+        if ( c <= 0x7F ) {      // short circuit ASCII
+            utf8Count = 0;
+            return c;
+        }
+        else {
+            if ( utf8Count < sizeof( utf8String ) - 1 ) {
+                utf8String[ utf8Count++ ] = c;
+                utf8String[ utf8Count ] = 0;
+                copyString8to32( unicodeChar, utf8String, 2, ucharCount, errorCode );
+                if ( ucharCount && errorCode == 0 ) {
+                    utf8Count = 0;
+                    return unicodeChar[0];
+                }
+            }
+            else {
+                // this really shouldn't happen ... got four bytes but no UTF-8 character ...
+                utf8Count = 0;
+            }
+        }
+    }
+#endif
 }
 
 #ifndef _WIN32
