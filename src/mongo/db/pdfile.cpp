f4034d2160f1d198cd8bd44286e8a8357dbbd648
Tad Marshall <tad@10gen.com>
2012-10-25 09:43:29 -0400

    SERVER-7438 remove unused 'extentLoc' argument from two routines
    Remove the unused 'extentLoc' argument from NamespaceDetails::alloc()
    and outOfSpace().

--- src/mongo/db/pdfile.cpp
+++ src/mongo/db/pdfile.cpp
@@ -1325,18 +1325,18 @@ namespace mongo {
         }
     }
 
-    NOINLINE_DECL DiskLoc outOfSpace(const char *ns, NamespaceDetails *d, int lenWHdr, bool god, DiskLoc extentLoc) {
+    NOINLINE_DECL DiskLoc outOfSpace(const char *ns, NamespaceDetails *d, int lenWHdr, bool god) {
         DiskLoc loc;
         if ( ! d->isCapped() ) { // size capped doesn't grow
             LOG(1) << "allocating new extent for " << ns << " padding:" << d->paddingFactor() << " lenWHdr: " << lenWHdr << endl;
             cc().database()->allocExtent(ns, Extent::followupSize(lenWHdr, d->lastExtentSize), false, !god);
-            loc = d->alloc(ns, lenWHdr, extentLoc);
+            loc = d->alloc(ns, lenWHdr);
             if ( loc.isNull() ) {
                 log() << "warning: alloc() failed after allocating new extent. lenWHdr: " << lenWHdr << " last extent size:" << d->lastExtentSize << "; trying again\n";
                 for ( int z=0; z<10 && lenWHdr > d->lastExtentSize; z++ ) {
                     log() << "try #" << z << endl;
                     cc().database()->allocExtent(ns, Extent::followupSize(lenWHdr, d->lastExtentSize), false, !god);
-                    loc = d->alloc(ns, lenWHdr, extentLoc);
+                    loc = d->alloc(ns, lenWHdr);
                     if ( ! loc.isNull() )
                         break;
                 }
@@ -1349,10 +1349,9 @@ namespace mongo {
       * @return null loc if out of space 
       */
     DiskLoc allocateSpaceForANewRecord(const char *ns, NamespaceDetails *d, int lenWHdr, bool god) {
-        DiskLoc extentLoc;
-        DiskLoc loc = d->alloc(ns, lenWHdr, extentLoc);
+        DiskLoc loc = d->alloc(ns, lenWHdr);
         if ( loc.isNull() ) {
-            loc = outOfSpace(ns, d, lenWHdr, god, extentLoc);
+            loc = outOfSpace(ns, d, lenWHdr, god);
         }
         return loc;
     }
@@ -1633,9 +1632,8 @@ namespace mongo {
         RARELY verify( d == nsdetails(ns) );
         DEV verify( d == nsdetails(ns) );
 
-        DiskLoc extentLoc;
         int lenWHdr = len + Record::HeaderSize;
-        DiskLoc loc = d->alloc(ns, lenWHdr, extentLoc);
+        DiskLoc loc = d->alloc(ns, lenWHdr);
         verify( !loc.isNull() );
 
         Record *r = loc.rec();
