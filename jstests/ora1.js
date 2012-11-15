28c938b612b083aae5247abc80b97944f927f3da
Tad Marshall <tad@10gen.com>
2012-11-15 16:46:09 -0500

    Tweaked jstests/ora1.js for debugging

--- jstests/ora1.js
+++ jstests/ora1.js
@@ -1,11 +1,7 @@
-var t = db.jstests_ora;
-
-// $where
-t.drop();
-for (var i = 0; i < 10; i += 1) {
-    t.save({x: i, y: 10 - i});
-}
-assert.eq.automsg("1", "t.find({$or: [{$where: 'this.x === 2'}]}).count()");
-assert.eq.automsg("2", "t.find({$or: [{$where: 'this.x === 2'}, {$where: 'this.y === 2'}]}).count()");
-assert.eq.automsg("1", "t.find({$or: [{$where: 'this.x === 2'}, {$where: 'this.y === 8'}]}).count()");
-assert.eq.automsg("10", "t.find({$or: [{$where: 'this.x === 2'}, {x: {$ne: 2}}]}).count()");
+var t = db.jstests_ora;                                                                                // #1
+t.drop();                                                                                              // #2
+for (var i = 0; i < 10; i += 1) { t.save({x: i, y: 10 - i}); }                                         // #3
+assert.eq.automsg("1", "t.find({$or: [{$where: 'this.x === 2'}]}).count()");                           // #4
+assert.eq.automsg("2", "t.find({$or: [{$where: 'this.x === 2'}, {$where: 'this.y === 2'}]}).count()"); // #5
+assert.eq.automsg("1", "t.find({$or: [{$where: 'this.x === 2'}, {$where: 'this.y === 8'}]}).count()"); // #6
+assert.eq.automsg("10", "t.find({$or: [{$where: 'this.x === 2'}, {x: {$ne: 2}}]}).count()");           // #7
