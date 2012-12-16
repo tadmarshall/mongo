bede653a945c2761a6098279b2982c5cb366b6b1
Tad Marshall <tad@10gen.com>
2012-12-16 14:45:01 -0500

    Improved CRT hook

--- src/mongo/util/platform_init.cpp
+++ src/mongo/util/platform_init.cpp
@@ -14,36 +14,24 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <iosfwd>
+//#include <iosfwd>
+#ifdef _WIN32
+#include <crtdbg.h>
 #include <stdlib.h>
+#endif
 
 #include "mongo/base/init.h"
-#include "mongo/util/log.h"
+//#include "mongo/util/log.h"
 #include "mongo/util/stacktrace.h"
 
 #ifdef _WIN32
 
 namespace mongo {
 
-    int crtDebugCallback(int debugHookCode, char* message, int* hook_retval) {
-        log() << "*** C runtime error: " << message << std::endl;
-        printStackTrace();
-        *hook_retval = 0;   // 0 == continue, 1 == CrtDebugBreak
-        return 1;           // 0 == not handled, non-0 == handled
-    }
-
     MONGO_INITIALIZER(Behaviors_Win32)(InitializerContext*) {
 
         // do not display dialog on abort()
         _set_abort_behavior(0, _CALL_REPORTFAULT | _WRITE_ABORT_MSG);
-
-        // send C runtime debug output to stderr
-        //_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-        //_CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-        //_CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-        //_CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-        //_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-        //_CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
 
         // hook the C runtime's error display
         _CrtSetReportHook(crtDebugCallback);
