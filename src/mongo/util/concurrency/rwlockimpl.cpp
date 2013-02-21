4d28bf2d25f2103dc882ce80c740ebb91f3efd59
Tad Marshall <tad@10gen.com>
2013-02-21 15:00:12 -0500

    SERVER-8488 Reintroduce Windows XP support

--- src/mongo/util/concurrency/rwlockimpl.cpp
+++ src/mongo/util/concurrency/rwlockimpl.cpp
@@ -16,12 +16,8 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include "mongo/platform/basic.h"
 
-#if defined(_WIN32)
-#define WIN32_LEAN_AND_MEAN
-#define NOMINMAX
-#include <windows.h>
-#endif
 #include <map>
 #include <set>
 #include <boost/version.hpp>
