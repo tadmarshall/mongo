b5a894ccbe4e913adcb7d5642629889c9484000e
Tad Marshall <tad@10gen.com>
2013-04-07 05:37:38 -0400

    more work on test

--- jstests/block_check_supported.js
+++ jstests/block_check_supported.js
@@ -3,6 +3,7 @@
 // db.serverStatus().indexCounters.
 // Related to SERVER-9242.
 
+jsTest.log("TESTING workingSet and indexCounters portions of serverStatus");
 var hostInfo = db.hostInfo();
 var isXP = (hostInfo.os.name == 'Windows XP') ? true : false;
 
@@ -10,5 +11,29 @@ var blockDB = db.getSiblingDB('block_check_supported');
 blockDB.dropDatabase();
 
 var serverStatus = blockDB.serverStatus({workingSet:1});
+if (!serverStatus) {
+    doassert("Test FAILED: db.serverStatus({workingSet:1}) did not return a value");
+}
+if (!serverStatus.workingSet) {
+    doassert("Test FAILED: db.serverStatus({workingSet:1}).workingSet was not returned");
+}
+if (!serverStatus.indexCounters) {
+    doassert("Test FAILED: db.serverStatus().indexCounters was not returned");
+}
+var workingSet = serverStatus.workingSet;
+var indexCounters_1 = serverStatus.indexCounters;
+
+//isXP = true;
+if (isXP) {
+    var expectedResult = { info: 'not supported' };
+    jsTest.log('Testing on Windows XP -- expecting ' + tojsononeline(expectedResult));
+    assert.eq(workingSet,
+              expectedResult,
+              'Test FAILED: db.serverStatus({workingSet:1}).workingSet' +
+              ' did not return the expected value: expected ' + tojsononeline(expectedResult) +
+              ', got ' + tojsononeline(workingSet));
+}
+
+
 
 
