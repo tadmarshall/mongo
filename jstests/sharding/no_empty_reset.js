96d7f315ec1dbec353dcb8587a9fb6b8811482f8
Tad Marshall <tad@10gen.com>
2013-05-10 12:46:27 -0400

    SERVER-9638 Handle case of previous moveChunk not finished yet
    Depending on the speeds of operations in the shards, it is possible that
    the "to" shard has not finished a migration when we try the next one.  Use
    assert.soon() to wait a little and retry when this happens.

--- jstests/sharding/no_empty_reset.js
+++ jstests/sharding/no_empty_reset.js
@@ -12,7 +12,7 @@ for( var i = -10; i < 10; i++ )
    
 st.shardColl( coll, { _id : 1 }, { _id : 0 } )
 
-jsTestLog( "Sharded setup complete" )
+jsTestLog("Sharded setup complete (but moveChunk may still be in progress)");
 
 st.printShardingStatus()
 
@@ -29,7 +29,25 @@ var fullShard = st.getShard( coll, { _id : 1 } )
 var emptyShard = st.getShard( coll, { _id : -1 } )
 
 var admin = st.s.getDB( "admin" )
-printjson( admin.runCommand({ moveChunk : "" + coll, find : { _id : -1 }, to : fullShard.shardName, _waitForDelete : true }) )
+var mcResult;
+assert.soon(
+    function () {
+        mcResult = admin.runCommand( { moveChunk: "" + coll,
+                                       find: { _id: -1 },
+                                       to: fullShard.shardName,
+                                       _waitForDelete: true } );
+        return mcResult && mcResult.ok;
+    },
+    "Setup FAILURE: " +
+            "Unable to move chunk from " + emptyShard.shardName +
+            " to " + fullShard.shardName +
+            ": result = " + tojson(mcResult),
+    10 * 1000,  /* timeout in ms */
+    100         /* interval in ms */
+);
+jsTestLog("Setup progress: moveChunk (all remaining) from " + emptyShard.shardName +
+          " to " + fullShard.shardName +
+          ": result = " + tojson(mcResult));
 
 jsTestLog( "Resetting shard version via first mongos..." )
 
@@ -48,4 +66,5 @@ st.printShardingStatus()
 
 assert.eq( 0, emptyColl.find().itcount() )
 
-
+jsTestLog("SUCCESS!  Stopping ShardingTest");
+st.stop();
