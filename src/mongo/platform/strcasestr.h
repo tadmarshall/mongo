255943c1acff87c6fe2ae17dd0b3cb6319e4f7c5
Tad Marshall <tad@10gen.com>
2013-06-06 17:04:29 -0400

    SERVER-9786 Link to strcasestr at runtime for Solaris
    For the Solaris/SmartOS build, do not make direct call to strcasestr,
    which is present in Solaris 11 but not in Solaris 10.  Instead, see if
    if is available in libc.so.1 at runtime and either call it or call an
    emulation.  Use the emulated version on Windows, which is also missing
    this function.

---
+++ src/mongo/platform/strcasestr.h
@@ -0,0 +1,59 @@
+/*    Copyright 2013 10gen Inc.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+
+#pragma once
+
+#if defined(_WIN32)
+
+// Windows will call the emulation of strcasestr()
+//
+namespace mongo {
+namespace pal {
+namespace emulation {
+    extern const char* strcasestr(const char* haystack, const char* needle);
+}
+}
+    using pal::emulation::strcasestr;
+}
+
+#elif defined(__sunos__)
+
+namespace mongo {
+    /**
+     * strcasestr -- case-insensitive search for a substring within another string.
+     *
+     * @param haystack      ptr to C-string to search
+     * @param needle        ptr to C-string to try to find within 'haystack'
+     * @return              ptr to start of 'needle' within 'haystack' if found, NULL otherwise
+     */
+    typedef const char* (*StrCaseStrFunc)(const char* haystack, const char* needle);
+namespace pal {
+namespace dynamic {
+    extern StrCaseStrFunc strcasestr;
+}
+}
+    using pal::dynamic::strcasestr;
+}
+
+#else
+
+// Other platforms will call the system strcasestr() directly
+//
+#include <cstring>
+namespace mongo {
+    using ::strcasestr;
+}
+
+#endif
--- src/mongo/util/platform_init.cpp
+++ src/mongo/util/platform_init.cpp
@@ -14,15 +14,25 @@
 *    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#ifdef _WIN32
+#if defined(_WIN32)
 #include <crtdbg.h>
 #include <stdlib.h>
+
+#include "mongo/util/stacktrace.h"
+#endif
+
+#if defined(__sunos__)
+#include <dlfcn.h>
+#include <string>
+
+#include "mongo/base/status.h"
+#include "mongo/platform/strcasestr.h"
+#include "mongo/util/log.h" // only needed if logging (debugging)
 #endif
 
 #include "mongo/base/init.h"
-#include "mongo/util/stacktrace.h"
 
-#ifdef _WIN32
+#if defined(_WIN32)
 
 namespace mongo {
 
@@ -40,3 +50,45 @@ namespace mongo {
 } // namespace mongo
 
 #endif // _WIN32
+
+#if defined(__sunos__)
+
+namespace mongo {
+
+    MONGO_INITIALIZER(SolarisDynamicLinks)(InitializerContext*) {
+
+    /*    Support run-time dynamic linking to functions that may or may not be present.
+     *
+     *    Some functions are available on Solaris 11 but not on Solaris 10.  In order to
+     *    ship a single binary that runs on both versions, we do not generate load-time
+     *    references to these functions but instead probe for them at run-time and either
+     *    call them if available of provide fallback behavior if not available.
+     */
+
+        // try to find some functions (should be in libc.so.1 if found)
+        static const char* libraryName = "/lib/64/libc.so.1";
+        void* libraryHandle = dlopen(libraryName, RTLD_LAZY);
+        //log() << "Library '" << libraryName << "' "
+        //      << (std::string((libraryHandle != NULL) ? "was" : "was NOT" ))
+        //      << " found" << std::endl;
+        if (libraryHandle == NULL) {
+            return Status::OK();
+            //return Status(ErrorCodes::InternalError, "Library not found");
+        }
+        static const char* functionName = "strcasestr";
+        void* functionAddress = dlsym(libraryHandle, functionName);
+
+        // note whether or not it was found
+        //log() << "Symbol '" << functionName << "' "
+        //      << (std::string((functionAddress != NULL) ? "was" : "was NOT" ))
+        //      << " found in '" << libraryName << "'" << std::endl;
+
+        if (functionAddress != NULL) {
+            dynamic::strcasestr = functionAddress;
+        }
+        return Status::OK();
+    }
+
+} // namespace mongo
+
+#endif // __sunos__
